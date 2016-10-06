1e6e17a740c9ca6f79594193fc2b4b9475550614
Spencer Gouthro <spencergouthro@Spencers-Air.eastlink.ca>
2016-10-06 14:03:17 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+/*	ENTER YOUR APP'S JAVASCRIPT CODE HERE!	*/
+
+// this function fires at the ready state, which is when the DOM is
+// ready for Javascript to execute
+$(document).ready(function() {
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAerdBWELDzhmZKU6Og7wgnpYqeJMqCMkY",
+    authDomain: "livecodetodo-f5342.firebaseapp.com",
+    databaseURL: "https://livecodetodo-f5342.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "756515170911"
+  };
+  firebase.initializeApp(config);
+
+
+	// @NOTE: it's probably a good idea to place your event 
+	//		  listeners in here :)
+});
+
